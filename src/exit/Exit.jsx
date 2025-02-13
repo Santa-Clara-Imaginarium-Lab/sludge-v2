ab429f0da468ecbce4a475e7ccf46ad44f81e9ca
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2025-02-13 01:39:18 -0800

    Login page

---
+++ src/exit/Exit.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Exit = () => {
+
+    return ( 
+        <div className='container'>
+            <h1>Exit</h1>
+        </div>
+    );
+};
+
+export default Exit;
