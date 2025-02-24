66e1fc09e8c1c10fb36c68716c16413a34e33b3a
Neha Sawant <109304726+nehasawant4@users.noreply.github.com>
2025-02-24 13:24:51 -0800

    prompt

--- src/popQuiz/Prompt3.jsx
+++ src/popQuiz/Prompt3.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../postTestSurvey/PostTestSurvey.css'; 
 import { useNavigate } from 'react-router-dom';
+import stopSign from '../image/STOP.jpg';
 
 function Prompt3() {
     const navigate = useNavigate();
@@ -11,6 +12,12 @@ function Prompt3() {
     
     return (
         <div className="container">
+            <h1 style={{ color: "red", fontSize: "2rem" }}>STOP</h1>
+            <img 
+                src={stopSign} 
+                alt="Stop Sign" 
+                style={{ width: "150px", height: "150px", marginBottom: "20px" }} 
+            />
             <h1>Please call the researcher for further instructions before proceeding</h1>
             <button className="submit-button" onClick={handleNavigate}>Proceed</button>
         </div>
