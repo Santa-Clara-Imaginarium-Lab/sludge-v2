0e4ed4fd6ccc35dfa3ed3712db5954061d017a89
jasonserran0519 <146367145+jasonserran0519@users.noreply.github.com>
2025-02-24 16:15:54 -0800

    bigger-vid

--- src/setup/Prompt1.jsx
+++ src/setup/Prompt1.jsx
@@ -14,7 +14,7 @@ function Prompt1() {
     
     return (
         <div className="container">
-            {/* <h1>Pre-Test Survey</h1> */}
+            <h1>Survey</h1>
             <button className="submit-button" onClick={handleNavigate} disabled={loading}>
                 {loading ? "Proceeding..." : "Proceed"}
             </button>
